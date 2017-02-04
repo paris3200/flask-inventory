f5b3a389bf295bc8f847885b9f9d799e9414b67c
gigabites <8arcena@gmail.com>
2017-02-04 11:46:18 -0800

    decode UTF-8 response when json.load()

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -81,7 +81,7 @@ class TestTagManagement(BaseTestCase):
             self.assertIn(str.encode(test_string), response.data)
             response = self.client.get(
                 '/api/categories')
-            the_cat_with_tag = json.loads(str(response.data))
+            the_cat_with_tag = json.loads(response.data.decode("utf-8"))
             self.assertEqual(
                 the_cat_with_tag[0]['tags'][0]['name'],
                 tag_in_cat.strip().upper())
